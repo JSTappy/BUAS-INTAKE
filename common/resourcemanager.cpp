5a8844145b6ed565c455d727562473379bec035b
Joas Sahetapy <joassahetapy@gmail.com>
2025-05-14 23:25:01 +0200

    Commenting
    + Adding comments to remsman

--- common/resourcemanager.cpp
+++ common/resourcemanager.cpp
@@ -14,10 +14,13 @@ ResourceManager::~ResourceManager()
 
 Sprite* ResourceManager::GetTarga(const std::string& fileName) //Getting Targas
 {
+	//If the targa has already been added to the list, return it
 	if (_targas[fileName] != NULL)
 	{
 		return _targas[fileName];
 	}
+
+	//Make a new sprite, load it and put it in the targas list
 	Sprite* s = new Sprite(fileName);
 	s->loadTGA(fileName);
 	_targas[fileName] = s;
