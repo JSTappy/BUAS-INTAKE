4460809e6958b837d231302fb0207ac7f81649a5
Joas Sahetapy <joassahetapy@gmail.com>
2025-05-14 12:16:38 +0200

    Adding Comments
    +  Adding MyScene comments

--- collidius/myscene.cpp
+++ collidius/myscene.cpp
@@ -4,11 +4,17 @@
 
 MyScene::MyScene() : Scene()
 {
+	//Initiate the timer
 	_startTimer = new Timer();
 	this->AddChild(_startTimer);
+
+	//Add the layers
 	_layer1 = new MyEntity();
+
+	//Set the background sprite
 	_layer1->SetSprite("assets/sprites/bg.tga");
 	_layer1->position = glm::vec3(WIDTH / 2, HEIGHT / 2, 0);
+
 	_layer2 = new MyEntity();
 	_layer3 = new MyEntity();
 
@@ -16,51 +22,56 @@ MyScene::MyScene() : Scene()
 	this->AddChild(_layer2);
 	this->AddChild(_layer3);
 
+	//Add the UIWindow
 	_uiDisplay = new UIWindow();
 	_uiDisplay->SetSprite("assets/sprites/uimockup.tga");
 	_uiDisplay->position = glm::vec3(WIDTH / 2, HEIGHT - 64, 0.0f);
 	_layer3->AddChild(_uiDisplay);
 	
+	//Add Player 1
 	_player1 = new Player(1, 74, 40, 9, 44, 0, 10);
 	_player1->SetSprite("assets/sprites/kchar.tga");
-	_player1->text = _uiDisplay->playerOneText;
+	_player1->text = _uiDisplay->playerOneText; //Set the player one text to the ui player one text so it can render on layer 3
 	_player1->UpdateHealthText();
 	_player1->position = glm::vec3(400, 300, 0);
 	_player1->SetStartPos();
-	_player1->AssignActionKey(KEY_Q);
+	_player1->AssignActionKey(KEY_Q); //The action key used to perform actions
 	_layer2->AddChild(_player1);
 	_gameEntities.push_back(_player1);
 
+	//Add Player 1
 	_player2 = new Player(2, 97, 35, 16, 36, 0, 10);
 	_player2->SetSprite("assets/sprites/jchar.tga");
-	_player2->text = _uiDisplay->playerTwoText;
+	_player2->text = _uiDisplay->playerTwoText; //Set the player two text to the ui player two text so it can render on layer 3
 	_player2->UpdateHealthText();
 	_player2->position = glm::vec3(250, 500, 0);
 	_player2->SetStartPos();
-	_player2->AssignActionKey(KEY_E);
+	_player2->AssignActionKey(KEY_E); //The action key used to perform actions
 	_layer2->AddChild(_player2);
 	_gameEntities.push_back(_player2);
 
-	_enemy = new Enemy(3, 1, 30, 15, 60, 0, 10);
+	//Add the Enemy
+	_enemy = new Enemy(3, 1063, 30, 15, 60, 0, 10);
 	_enemy->SetSprite("assets/sprites/gorilla.tga");
-	_enemy->text = _uiDisplay->enemyText;
+	_enemy->text = _uiDisplay->enemyText; //Set the enemy text to the ui enemy text so it can render on layer 3
 	_enemy->UpdateHealthText();
 	_enemy->position = glm::vec3(950, 360, 0);
 	_enemy->SetStartPos();
 	_layer2->AddChild(_enemy);
 	_gameEntities.push_back(_enemy);
 
+	//Add these game entities to the turnmanager game entity list
 	for (size_t i = 0; i < _gameEntities.size(); i++)
 	{
 		TurnManager::Instance()->AddGameEntities(_gameEntities[i]);
 	}
-	TurnManager::Instance()->battleText = _uiDisplay->text;
-	TurnManager::Instance()->battleText->text = _textStrings[0];
+	TurnManager::Instance()->battleText = _uiDisplay->text; //Set turnmanager battle text to uiwindow battle text so it can render on layer 3
+	TurnManager::Instance()->battleText->text = _introDialogue[0];
+
+	//Start the timer
 	_startTimer->StartTimer();
 	_startTimer->isPlaying = true;
 }
-
-
 
 MyScene::~MyScene()
 {
@@ -69,34 +80,36 @@ MyScene::~MyScene()
 
 void MyScene::Update(float deltaTime)
 {
-	if (!setupComplete)
+	if (!setupComplete) { HandleTutorial(); return; } //Handle the tutorial if it has not been completed yet
+	if (GetInput()->GetKeyDown(KEY_Z)) { TurnManager::Instance()->DisplayStats(); } //Display stats if the Z key is pressed
+	if (GetInput()->GetKeyDown(KEY_T)) //Display the hitboxes if the T key is pressed
 	{
-		HandleTutorial();
-		return;
-	}
-	if (GetInput()->GetKeyDown(KEY_Z)) { TurnManager::Instance()->DisplayStats(); }
-	if (GetInput()->GetKeyDown(KEY_T))
-	{
-		displayHitboxes = !displayHitboxes;
+		_displayHitboxes = !_displayHitboxes;
 		for (size_t i = 0; i < _gameEntities.size(); i++)
 		{
-			_gameEntities[i]->ToggleHitboxDisplay(displayHitboxes);
+			_gameEntities[i]->ToggleHitboxDisplay(_displayHitboxes); //Toggle the hitboxes
 		}
 	}
+
+	//Check if a game entity has died
 	for (size_t i = 0; i < _gameEntities.size(); i++)
 	{
+		//If the health of the game entity is 0, kill it
 		if (_gameEntities[i]->health <= 0)
 		{
 			std::cout << "Game Entity Defeated" << std::endl;
 			_gameEntities[i]->alive = false;
 			_layer2->RemoveChild(_gameEntities[i]);
-			TurnManager::Instance()->KillEntity(_gameEntities[i]);
-			delete _gameEntities[i];
+			TurnManager::Instance()->KillEntity(_gameEntities[i]); //Remove the game entity from the turnmanagers list as well
+			delete _gameEntities[i]; //Delete it here
 			_gameEntities[i] = nullptr;
-			_gameEntities.erase(_gameEntities.begin() + i);
+			_gameEntities.erase(_gameEntities.begin() + i); //remove it from the list
 
+			//TODO complete game screen
 		}
 	}
+
+	//Play the music if it is not playing yet
 	if (_bgm->isPlaying())return;
 	_bgm->setLooping(true);
 	_bgm->setVolume(0.5f);
@@ -104,33 +117,42 @@ void MyScene::Update(float deltaTime)
 }
 void MyScene::HandleTutorial()
 {
-	if (GetInput()->GetKeyDown(SPACE))
-	{
-		CompleteTutorial();
-		return;
-	}
-	if (_textinterval <= _startTimer->GetSeconds() && _textcount < _textStrings.size() && _textinterval < 24.39f)
+	if (GetInput()->GetKeyDown(SPACE)) { CompleteTutorial(); return; } //Skip the tutorial when pressing Space Key
+
+	/*	If the textinterval is below or equal to the amount of seconds the start timer
+		And the textcount is still smaller than the intro dialogue strings size
+		And if the text interval is below 24.39f
+
+		Switch to the next intro dialogue string
+	*/
+	if (_textinterval <= _startTimer->GetSeconds() && _textcount < _introDialogue.size() && _textinterval < 24.39f)
 	{
 		_textcount++;
-		TurnManager::Instance()->battleText->text = _textStrings[_textcount];
+		TurnManager::Instance()->battleText->text = _introDialogue[_textcount];
 		_textinterval += 2.71f;
 		return;
 	}
+	
+	//If the text interval is smaller than 24.39f, return because the if statement above this does not get called every frame
 	if (_textinterval < 24.39f)return;
-	if (_startTimer->GetSeconds() <= 24.4f && _startTimer->isPlaying)
+
+	//Play the intro once the last string is on screen and the time is below the interval
+	if (_startTimer->GetSeconds() <= 24.39f)
 	{
+		//Play the intro if it isnt playing already
 		if (_intro->isPlaying())return;
 		_intro->play();
 		_intro->setVolume(0.8f);
 		return;
 	}
-	if (!setupComplete) { CompleteTutorial(); }
+	if (!setupComplete)CompleteTutorial(); //Complete the tutorial if the timer exceeds the 24.39f threshold
 }
 void MyScene::CompleteTutorial() 
 {
+	//Start the game
 	TurnManager::Instance()->StartGame(); 
-	_startTimer->StopTimer();  
+	_startTimer->StopTimer(); //Stop the timer
 	_startTimer->isPlaying = false;
-	if (_intro->isPlaying()) { _intro->stop(); }
+	if (_intro->isPlaying())_intro->stop(); //Stop the music
 	setupComplete = true;
 }
--- collidius/myscene.h
+++ collidius/myscene.h
@@ -4,7 +4,6 @@
 #include "scene.h"
 #include "game.h"
 #include "entity.h"
-#include "myentity.h"
 #include "text.h"
 
 #include "GameEntity.h"
@@ -29,43 +28,71 @@ public:
 	/// @return void
 	virtual void Update(float deltaTime);
 
-	bool displayHitboxes = false;
 	
 private:
+
+	///@brief Complete the tutorial if it has not been completed already
+	void CompleteTutorial();
+
+	///@brief Handle the tutorial with dialogue and music
+	void HandleTutorial();
+	
+	///@brief The background
+	Entity* _layer1;
+
+	///@brief The entity containing the game entities and projectiles
+	Entity* _layer2;
+
+	///@brief The entity containing the UI at the bottom of the screen
+	Entity* _layer3;
+
+	///@brief The first player, Lee
 	Player* _player1;
+
+	///@brief The second player, Cole
 	Player* _player2;
+
+	///@brief The enemy, Sean
 	Enemy* _enemy;
 
+	///@brief The window at the bottom of the screen with the HP and battle text
 	UIWindow* _uiDisplay;
-	MyEntity* _battleText;
-	MyEntity* _layer1;
-	MyEntity* _layer2;
-	MyEntity* _layer3;
 
+	///@brief Timer for the intro
 	Timer* _startTimer;
 
-	bool setupComplete;
-
+	///@brief the background music that will loop
 	Audio::Sound* _bgm = new Audio::Sound("assets/audio/bgm.ogg", Audio::Sound::Type::Music);
+
+	///@brief the intro to the background music of 2.71f seconds
 	Audio::Sound* _intro = new Audio::Sound("assets/audio/intro.ogg", Audio::Sound::Type::Sound);
 
-	bool _skippedTutorial;
+	///@brief Check if the hiboxes should be displayed
+	bool _displayHitboxes = false;
 
-	void CompleteTutorial();
-	void HandleTutorial();
+	///@brief Check if the game has been set up
+	bool setupComplete;
+
+	///@brief The amount of seconds the timer should reach before moving on to the next index in the _introDialogue
 	float _textinterval = 0;
+
+	///@brief the index of the introdialogue
 	int _textcount = 0;
+
+	///@brief the list of gameEntities
 	std::vector<GameEntity*> _gameEntities;
-	std::vector<const char*> _textStrings
+
+	///@brief The dialogue that will play as a tutorial
+	std::vector<const char*> _introDialogue
 	{
 		"...",
 		"'Its just us two left, huh Cole?'",
 		"'I guess you're right... Lee.'",
-		"'This gorilla... Sean is something else'",
-		"'Alright, we can choose to attack, defend or use an item'",
+		"'This gorilla... Sean is something else.'",
+		"'Alright, we can choose to attack, defend or use an item.'",
 		"'We jump to hit the action block and choose what we do right?'",
 		"'Exactly! when we do, we can choose from 4 actions.'",
-		"'We deal massive damage if we use our actions'",
+		"'We deal massive damage if we use our actions.'",
 		"'Let's get him Cole! We humans have no limits!'",
 		"'Damn right Lee! Let's charge together at full power!'",
 	};
