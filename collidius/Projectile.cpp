a28e6aa080584ede82d7a1d8f53881f49cc8a13b
Joas Sahetapy <joassahetapy@gmail.com>
2025-05-14 11:26:46 +0200

    Adding Projectile Comments

--- collidius/Projectile.cpp
+++ collidius/Projectile.cpp
@@ -6,39 +6,41 @@ Projectile::Projectile(glm::vec3 startPosition, float newspeed, bool displayHitb
     _startPosition = startPosition;
     speed = newspeed;
 
-    hitBox = new MyEntity();
+    _direction = glm::vec3(0);
+    _endPosition = glm::vec3(0);
+
+    //Set up the hitbox
+    hitBox = new Entity();
     hitBox->position = this->position;
     hitBox->scale = glm::vec3(0.5f, 0.5f, 0.0f);
     this->AddChild(hitBox);
-    if (displayHitbox)
-    { 
-        hitBox->SetSprite("assets/sprites/hitbox.tga"); 
-    }
-    else 
-    { 
-        hitBox->sprite = nullptr; 
-    }
+
+    //Set the hitbox sprite if it should be displayed
+    if (displayHitbox) { hitBox->SetSprite("assets/sprites/hitbox.tga"); return; }
+    hitBox->sprite = nullptr; 
 }
 
 Projectile::~Projectile()
 {
-	
+    //Destroy the hitbox
+    this->RemoveChild(hitBox);
+    delete hitBox;
+    hitBox = nullptr;
 }
 
 void Projectile::SetTarget(glm::vec3 targetPosition)
 {
     _endPosition = targetPosition;
-    _endPosition.y += 32;
-    _direction = glm::normalize(_endPosition - this->position); //get the vector towards the target (player)
+    _endPosition.y += 32; //Undershoot a tad bit to make it easier for the player to jump over
+    _direction = glm::normalize(_endPosition - this->position); //Get the normalized vector towards the target
 }
 
 void Projectile::Update(float deltaTime)
 {
+    //Make sure the hitbox follows this position
     hitBox->position = this->position;
-    this->position += _direction * speed * deltaTime; //move towards the target (player)
-    this->rotation -= speed / 50 * deltaTime; //rotate the star as visual
-
-    //std::cout << "Pos: " << this->position.x << " " << this->position.y << " " << this->position.z << std::endl;
+    this->position += _direction * speed * deltaTime; //Move towards the target
+    this->rotation -= speed / 50 * deltaTime; //Rotate the sprite for extra visual effect
 
 }
 
--- collidius/Projectile.h
+++ collidius/Projectile.h
@@ -1,12 +1,9 @@
 #ifndef PROJECTILE_H 
 #define PROJECTILE_H
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include "Entity.h"
 
-#include "MyEntity.h"
-
-class Projectile : public MyEntity
+class Projectile : public Entity
 {
 public:
 	/// @brief constructor
@@ -15,18 +12,29 @@ public:
 	/// @brief destructor
 	virtual ~Projectile();
 
-	void SetTarget(glm::vec3 targetPosition);
 	/// @brief update method
 	/// @param deltaTime
 	/// @return void
 	virtual void Update(float deltaTime); //update
 
+	///@brief Set the target position this projectile needs to go towards
+	///@param glm::vec3 targetPosition, the position of the target
+	void SetTarget(glm::vec3 targetPosition);
+
+	///@brief Set the target position this projectile needs to go towards
 	float speed;
-	MyEntity* hitBox;
+
+	///@brief Hitbox Sprite
+	Entity* hitBox;
 private:
 
+	///@brief The position this entity has to reach
 	glm::vec3 _endPosition;
+
+	///@brief The position this entity starts from
 	glm::vec3 _startPosition;
+
+	///@brief The direction this entity should go
 	glm::vec3 _direction;
 
 };
